2014d1f1929ba16d806c1bdc230c8f5831bad655
Tausif104 <tausifahmed49@gmail.com>
2022-04-06 01:22:14 +0600

    update upload

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import NextNProgress from 'nextjs-progressbar'
 import 'react-modal-video/css/modal-video.css'
 import '../styles/animate.css'
 import '../styles/globals.css'
-import '../styles/responsive.css'
 
 function MyApp({ Component, pageProps }) {
 	return (
