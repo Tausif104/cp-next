ae3d55961e5a29e1d6f2287d9c458b47f2f1afc3
Tausif104 <tausifahmed49@gmail.com>
2022-04-08 03:20:24 +0600

    new branch for cpanel

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	reactStrictMode: true,
-}
-
-module.exports = nextConfig
-
-// module.exports = {
-// 	basePath: '/cp-dev',
-// 	trailingSlash: true,
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+// 	reactStrictMode: true,
 // }
+
+// module.exports = nextConfig
+
+module.exports = {
+	basePath: '/cp-dev',
+	trailingSlash: true,
+}
