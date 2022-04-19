56cede387380fb611a6128fc2aa9618a57e25f09
tausif104 <tausifahmed49@gmail.com>
2022-04-20 02:53:32 +0600

    done

--- pages/post/[slug]/index.js
+++ pages/post/[slug]/index.js
@@ -18,7 +18,7 @@ const BlogDetails = ({ post, posts, categories }) => {
 
 	const { id, title, content, excerpt, link } = post[0]
 
-	const newContent = content?.rendered.replaceAll(link + '#', '#')
+	// const newContent = content?.rendered.replaceAll(link + '#', '#')
 
 	// refs
 	const nameRef = useRef()
@@ -123,7 +123,7 @@ const BlogDetails = ({ post, posts, categories }) => {
 										<div
 											className='content'
 											dangerouslySetInnerHTML={{
-												__html: newContent,
+												__html: content,
 											}}
 										></div>
 									)}
