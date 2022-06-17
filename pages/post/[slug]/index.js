d94e9ee5a686fb0258f8a5a64c2ea34087259929
tausif104 <tausifahmed49@gmail.com>
2022-06-18 01:59:13 +0600

    static site done

--- pages/post/[slug]/index.js
+++ pages/post/[slug]/index.js
@@ -9,252 +9,169 @@ import moment from 'moment'
 import BlogCard from '../../../components/blogPage/BlogCard/BlogCard'
 
 const BlogDetails = ({ post, posts, categories }) => {
-	const [comment, setComment] = useState({})
-	const [comments, setComments] = useState([])
-	const [loading, setLoading] = useState(false)
+  const [comment, setComment] = useState({})
+  const [comments, setComments] = useState([])
+  const [loading, setLoading] = useState(false)
 
-	const router = useRouter()
-	const { slug } = router.query
+  const router = useRouter()
+  const { slug } = router.query
 
-	const { id, title, content, excerpt, link } = post[0]
+  const { id, title, content, excerpt, link } = post[0]
 
-	// const newContent = content?.rendered.replaceAll(link + '#', '#')
+  // const newContent = content?.rendered.replaceAll(link + '#', '#')
+  console.log(title)
 
-	// refs
-	const nameRef = useRef()
-	const emailRef = useRef()
-	const commentRef = useRef()
+  // refs
+  const nameRef = useRef()
+  const emailRef = useRef()
+  const commentRef = useRef()
 
-	useEffect(() => {
-		setLoading(true)
-		const fetchAllComments = async () => {
-			const { data } = await axios(
-				`https://creativepeoplesdesign.com/admin/wp-json/wp/v2/comments?post=${id}`
-			)
-			setComments(data)
-			setLoading(false)
-		}
+  useEffect(() => {
+    setLoading(true)
+    const fetchAllComments = async () => {
+      const { data } = await axios(`https://creativepeoplesdesign.com/admin/wp-json/wp/v2/comments?post=${id}`)
+      setComments(data)
+      setLoading(false)
+    }
 
-		fetchAllComments()
-	}, [id, comment])
+    fetchAllComments()
+  }, [id, comment])
 
-	const handleCommentForm = async (e) => {
-		setLoading(true)
-		e.preventDefault()
+  const handleCommentForm = async (e) => {
+    setLoading(true)
+    e.preventDefault()
 
-		const commentObj = {
-			post: id,
-			author_name: nameRef.current.value,
-			author_email: emailRef.current.value,
-			content: commentRef.current.value,
-		}
+    const commentObj = {
+      post: id,
+      author_name: nameRef.current.value,
+      author_email: emailRef.current.value,
+      content: commentRef.current.value,
+    }
 
-		const { data } = await axios.post(
-			'https://creativepeoplesdesign.com/admin/wp-json/wp/v2/comments',
-			commentObj
-		)
+    const { data } = await axios.post('https://creativepeoplesdesign.com/admin/wp-json/wp/v2/comments', commentObj)
 
-		setComment(data)
-		setLoading(false)
+    setComment(data)
+    setLoading(false)
 
-		setComments([...comments, comment])
+    setComments([...comments, comment])
 
-		e.target.reset()
-	}
+    e.target.reset()
+  }
 
-	if (post[0]) {
-		return (
-			<Layout>
-				<Head>
-					<title>{title.rendered}</title>
-					<meta name='description' content={excerpt.rendered} />
-				</Head>
-				<section className='blog-details-page'>
-					<div className='container custom_container'>
-						<div className='row'>
-							<div className='col-lg-9'>
-								<div className='blog-details-post'>
-									{post[0]?.x_featured_media_original && (
-										<img
-											className='w-100'
-											src={
-												post[0]
-													?.x_featured_media_original
-											}
-											alt={title?.rendered}
-										/>
-									)}
-									<div className='blog-meta'>
-										<span>
-											<i className='far fa-clock me-2'></i>
-											{moment(post[0].date).format(
-												'MMM Do YY'
-											)}
-										</span>
-										<span>
-											<i className='far fa-user me-2'></i>{' '}
-											by{' '}
-											<Link
-												href={`/author/${post[0]._embedded.author[0].id}`}
-											>
-												<a className='text-capitalize'>
-													{
-														post[0]._embedded
-															.author[0].name
-													}
-												</a>
-											</Link>
-										</span>
-										<span>
-											<i className='far fa-comments me-2'></i>{' '}
-											{post[0]._embedded?.replies
-												? post[0]._embedded.replies[0]
-														.length
-												: 0}{' '}
-											Comments
-										</span>
-									</div>
-									<h1
-										dangerouslySetInnerHTML={{
-											__html: title?.rendered,
-										}}
-									></h1>
-									{content && (
-										<div
-											className='content'
-											dangerouslySetInnerHTML={{
-												__html: content.rendered,
-											}}
-										></div>
-									)}
-									<div className='row'>
-										<div className='col-lg-8'>
-											<div className='blog-form'>
-												<h3>Leave us a message</h3>
-												<form
-													onSubmit={handleCommentForm}
-												>
-													<div className='form-group'>
-														<input
-															ref={nameRef}
-															required
-															type='text'
-															className='form-control'
-															placeholder='Name'
-														/>
-													</div>
-													<div className='form-group'>
-														<input
-															ref={emailRef}
-															required
-															type='email'
-															className='form-control'
-															placeholder='email'
-														/>
-													</div>
-													<div className='form-group'>
-														<textarea
-															ref={commentRef}
-															className='form-control'
-															placeholder='Message'
-														></textarea>
-													</div>
-													<input
-														type='submit'
-														value={
-															loading
-																? 'Posting'
-																: 'Publish'
-														}
-													/>
-												</form>
-												{comments.length > 0 && (
-													<div className='comments-list'>
-														<h4>Comments</h4>
-														{comments.map((c) => (
-															<div
-																key={c.id}
-																className='comment-item'
-															>
-																<div className='comment-avatar'>
-																	<img
-																		src={
-																			c
-																				?.author_avatar_urls?.[96]
-																		}
-																		alt=''
-																	/>
-																</div>
-																<div className='comment-right'>
-																	<h4>
-																		{
-																			c.author_name
-																		}
-																	</h4>
-																	<div
-																		className='comment-body'
-																		dangerouslySetInnerHTML={{
-																			__html: c
-																				?.content
-																				?.rendered,
-																		}}
-																	></div>
-																	<p>
-																		{moment(
-																			c.date
-																		).format(
-																			'MMM Do YY'
-																		)}
-																	</p>
-																</div>
-															</div>
-														))}
-													</div>
-												)}
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className='col-lg-3'>
-								<BlogSidebar
-									post={post}
-									posts={posts}
-									categories={categories}
-								/>
-							</div>
-						</div>
-						<div className='row related_posts'>
-							<div className='col-lg-12 text-center'>
-								<div className='cre_cta_content'>
-									<h3 className='title'>
-										<span>Related Posts</span>
-									</h3>
-								</div>
-							</div>
-							{posts.slice(0, 3).map((post) => (
-								<BlogCard key={post.id} post={post} />
-							))}
-						</div>
-					</div>
-				</section>
-			</Layout>
-		)
-	}
+  if (post[0]) {
+    return (
+      <Layout>
+        <Head>
+          <title>{title.rendered}</title>
+          <meta name='description' content={excerpt.rendered} />
+        </Head>
+        <section className='blog-details-page'>
+          <div className='container custom_container'>
+            <div className='row'>
+              <div className='col-lg-9'>
+                <div className='blog-details-post'>
+                  {post[0]?.x_featured_media_original && <img className='w-100' src={post[0]?.x_featured_media_original} alt={title?.rendered} />}
+                  <div className='blog-meta'>
+                    <span>
+                      <i className='far fa-clock me-2'></i>
+                      {moment(post[0].date).format('MMM Do YY')}
+                    </span>
+                    <span>
+                      <i className='far fa-user me-2'></i> by{' '}
+                      <Link href={`/author/${post[0]._embedded.author[0].id}`}>
+                        <a className='text-capitalize'>{post[0]._embedded.author[0].name}</a>
+                      </Link>
+                    </span>
+                    <span>
+                      <i className='far fa-comments me-2'></i> {post[0]._embedded?.replies ? post[0]._embedded.replies[0].length : 0} Comments
+                    </span>
+                  </div>
+                  <h1
+                    dangerouslySetInnerHTML={{
+                      __html: title?.rendered,
+                    }}
+                  ></h1>
+                  {content && (
+                    <div
+                      className='content'
+                      dangerouslySetInnerHTML={{
+                        __html: content.rendered,
+                      }}
+                    ></div>
+                  )}
+                  <div className='row'>
+                    <div className='col-lg-8'>
+                      <div className='blog-form'>
+                        <h3>Leave us a message</h3>
+                        <form onSubmit={handleCommentForm}>
+                          <div className='form-group'>
+                            <input ref={nameRef} required type='text' className='form-control' placeholder='Name' />
+                          </div>
+                          <div className='form-group'>
+                            <input ref={emailRef} required type='email' className='form-control' placeholder='email' />
+                          </div>
+                          <div className='form-group'>
+                            <textarea ref={commentRef} className='form-control' placeholder='Message'></textarea>
+                          </div>
+                          <input type='submit' value={loading ? 'Posting' : 'Publish'} />
+                        </form>
+                        {comments.length > 0 && (
+                          <div className='comments-list'>
+                            <h4>Comments</h4>
+                            {comments.map((c) => (
+                              <div key={c.id} className='comment-item'>
+                                <div className='comment-avatar'>
+                                  <img src={c?.author_avatar_urls?.[96]} alt='' />
+                                </div>
+                                <div className='comment-right'>
+                                  <h4>{c.author_name}</h4>
+                                  <div
+                                    className='comment-body'
+                                    dangerouslySetInnerHTML={{
+                                      __html: c?.content?.rendered,
+                                    }}
+                                  ></div>
+                                  <p>{moment(c.date).format('MMM Do YY')}</p>
+                                </div>
+                              </div>
+                            ))}
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='col-lg-3'>
+                <BlogSidebar post={post} posts={posts} categories={categories} />
+              </div>
+            </div>
+            <div className='row related_posts'>
+              <div className='col-lg-12 text-center'>
+                <div className='cre_cta_content'>
+                  <h3 className='title'>
+                    <span>Related Posts</span>
+                  </h3>
+                </div>
+              </div>
+              {posts.slice(0, 3).map((post) => (
+                <BlogCard key={post.id} post={post} />
+              ))}
+            </div>
+          </div>
+        </section>
+      </Layout>
+    )
+  }
 }
 
-export const getServerSideProps = async (context) => {
-	const postsRes = await fetch(
-		'https://creativepeoplesdesign.com/admin/wp-json/wp/v2/posts?_embed'
-	)
-	const { data } = await axios.get(
-		`https://creativepeoplesdesign.com/admin/wp-json/wp/v2/posts?slug=${context.params.slug}&_embed`
-	)
+export const getServerSideProps = async (params) => {
+  const postsRes = await fetch('https://creativepeoplesdesign.com/admin/wp-json/wp/v2/posts?_embed')
+  const { data } = await axios.get(`https://creativepeoplesdesign.com/admin/wp-json/wp/v2/posts?slug=${params.slug}&_embed`)
+  const posts = await postsRes.json()
+  const post = await data
 
-	const posts = await postsRes.json()
-	const post = await data
-
-	return { props: { post, posts } }
+  return { props: { post, posts } }
 }
 
 export default BlogDetails
--- pages/project/[id]/index.js
+++ pages/project/[id]/index.js
@@ -4,51 +4,42 @@ import Layout from '../../../components/shared/Layout/Layout'
 import PageBanner from '../../../components/shared/PageBanner/PageBanner'
 
 const Project = ({ project }) => {
-	const router = useRouter()
-	const { id } = router.query
-	const { title, description, images, tags } = project
+  const router = useRouter()
+  const { id } = router.query
+  const { title, description, images, tags } = project
 
-	return (
-		<Layout>
-			<Head>
-				<title>{title}</title>
-			</Head>
-			<PageBanner title='Project Details' />
-			<section className='project-details-section'>
-				<div className='container custom_container'>
-					<div className='row'>
-						<div className='col'>
-							<img
-								className='img-fluid'
-								src={images?.hidpi}
-								alt={title}
-							/>
-							<h1>{title}</h1>
-							<div
-								className='project-details-content'
-								dangerouslySetInnerHTML={{
-									__html: description,
-								}}
-							/>
-							<ul>
-								{tags &&
-									tags.map((tag) => <li key={tag}>{tag}</li>)}
-							</ul>
-						</div>
-					</div>
-				</div>
-			</section>
-		</Layout>
-	)
+  return (
+    <Layout>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <PageBanner title='Project Details' />
+      <section className='project-details-section'>
+        <div className='container custom_container'>
+          <div className='row'>
+            <div className='col'>
+              <img className='img-fluid' src={images?.hidpi} alt={title} />
+              <h1>{title}</h1>
+              <div
+                className='project-details-content'
+                dangerouslySetInnerHTML={{
+                  __html: description,
+                }}
+              />
+              <ul>{tags && tags.map((tag) => <li key={tag}>{tag}</li>)}</ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
 export async function getServerSideProps(context) {
-	const res = await fetch(
-		`https://api.dribbble.com/v2/shots/${context.params.id}/?access_token=1d41a60e6a59faef44cabd36abcf7003292ec9a0a49fc2db833e2c537bcb86b5`
-	)
-	const project = await res.json()
+  const res = await fetch(`https://api.dribbble.com/v2/shots/${context.params.id}/?access_token=1d41a60e6a59faef44cabd36abcf7003292ec9a0a49fc2db833e2c537bcb86b5`)
+  const project = await res.json()
 
-	return { props: { project } }
+  return { props: { project }, revalidate: 10 }
 }
 
 export default Project
