a647140cd51457171f7f7bfca064039e875e7d9b
tausif104 <tausifahmed49@gmail.com>
2022-06-18 00:56:31 +0600

    added getStatic props

--- pages/blog.js
+++ pages/blog.js
@@ -7,108 +7,77 @@ import Link from 'next/link'
 import trimWords from 'trim-words'
 
 const Blog = ({ posts }) => {
-	const [visible, setVisible] = useState(4)
+  const [visible, setVisible] = useState(4)
 
-	const handleLoadMore = () => {
-		if (posts.length > visible) {
-			setVisible((prev) => prev + 4)
-		}
-	}
+  const handleLoadMore = () => {
+    if (posts.length > visible) {
+      setVisible((prev) => prev + 4)
+    }
+  }
 
-	return (
-		<Layout>
-			<Head>
-				<title>Learn Everything You Need To Know</title>
-				<meta
-					name='description'
-					content='If you are still confused and want to learn more about our product and services then look no further and go to our Blog Page.'
-				/>
-			</Head>
-			<PageBanner title='Blog' />
-			<div className='container custom_container padding_top padding_bottom'>
-				<div className='row'>
-					<div className='col-lg-9'>
-						<BlogListing posts={posts.slice(0, visible)} />
-						<div className='load-more-wrapper text-center mt-5'>
-							{posts.length > visible && (
-								<button
-									className='inline-btn'
-									onClick={handleLoadMore}
-								>
-									Load More
-								</button>
-							)}
-						</div>
-					</div>
-					<div className='col-lg-3'>
-						<aside>
-							<div className='recent-post'>
-								<h4>Recent Posts</h4>
-								<div className='recent-posts-list'>
-									{posts &&
-										posts.slice(0, 3).map((post) => (
-											<div
-												className='recent-post-item'
-												key={post.id}
-											>
-												{post.x_featured_media_original && (
-													<img
-														src={
-															post.x_featured_media_original
-														}
-														alt={
-															post.title.rendered
-														}
-													/>
-												)}
-												<div className='rp-content'>
-													<h5>
-														<Link
-															href={`/${post.slug}`}
-														>
-															<a
-																dangerouslySetInnerHTML={{
-																	__html: post
-																		.title
-																		.rendered,
-																}}
-															></a>
-														</Link>
-													</h5>
-													<div
-														dangerouslySetInnerHTML={{
-															__html: trimWords(
-																post.excerpt
-																	.rendered,
-																5,
-																'...'
-															),
-														}}
-													></div>
-												</div>
-											</div>
-										))}
-								</div>
-							</div>
-						</aside>
-					</div>
-				</div>
-			</div>
-		</Layout>
-	)
+  return (
+    <Layout>
+      <Head>
+        <title>Learn Everything You Need To Know</title>
+        <meta name='description' content='If you are still confused and want to learn more about our product and services then look no further and go to our Blog Page.' />
+      </Head>
+      <PageBanner title='Blog' />
+      <div className='container custom_container padding_top padding_bottom'>
+        <div className='row'>
+          <div className='col-lg-9'>
+            <BlogListing posts={posts.slice(0, visible)} />
+            <div className='load-more-wrapper text-center mt-5'>
+              {posts.length > visible && (
+                <button className='inline-btn' onClick={handleLoadMore}>
+                  Load More
+                </button>
+              )}
+            </div>
+          </div>
+          <div className='col-lg-3'>
+            <aside>
+              <div className='recent-post'>
+                <h4>Recent Posts</h4>
+                <div className='recent-posts-list'>
+                  {posts &&
+                    posts.slice(0, 3).map((post) => (
+                      <div className='recent-post-item' key={post.id}>
+                        {post.x_featured_media_original && <img src={post.x_featured_media_original} alt={post.title.rendered} />}
+                        <div className='rp-content'>
+                          <h5>
+                            <Link href={`/${post.slug}`}>
+                              <a
+                                dangerouslySetInnerHTML={{
+                                  __html: post.title.rendered,
+                                }}
+                              ></a>
+                            </Link>
+                          </h5>
+                          <div
+                            dangerouslySetInnerHTML={{
+                              __html: trimWords(post.excerpt.rendered, 5, '...'),
+                            }}
+                          ></div>
+                        </div>
+                      </div>
+                    ))}
+                </div>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
-export const getServerSideProps = async () => {
-	const res = await fetch(
-		'https://creativepeoplesdesign.com/admin/wp-json/wp/v2/posts?per_page=100&&_embed'
-	)
-	const catRes = await fetch(
-		'https://creativepeoplesdesign.com/admin/wp-json/wp/v2/categories'
-	)
-	const posts = await res.json()
-	const categories = await catRes.json()
+export const getStaticProps = async () => {
+  const res = await fetch('https://creativepeoplesdesign.com/admin/wp-json/wp/v2/posts?per_page=100&&_embed')
+  const catRes = await fetch('https://creativepeoplesdesign.com/admin/wp-json/wp/v2/categories')
+  const posts = await res.json()
+  const categories = await catRes.json()
 
-	return { props: { posts, categories } }
+  return { props: { posts, categories }, revalidate: 10 }
 }
 
 export default Blog
--- pages/projects.js
+++ pages/projects.js
@@ -5,35 +5,28 @@ import Layout from '../components/shared/Layout/Layout'
 import PageBanner from '../components/shared/PageBanner/PageBanner'
 
 const projects = ({ projects, links }) => {
-	return (
-		<Layout>
-			<Head>
-				<title>Fully Customer Satisfied Projects</title>
-				<meta
-					name='description'
-					content='We have delivered 1100+ customer-satisfied projects with smiles, happiness, joys, and charm. Click here and spread your joy with everyone!'
-				/>
-			</Head>
-			<PageBanner title='Projects' />
-			<ProjectList projects={projects} />
-			<LivelinkList links={links} />
-		</Layout>
-	)
+  return (
+    <Layout>
+      <Head>
+        <title>Fully Customer Satisfied Projects</title>
+        <meta name='description' content='We have delivered 1100+ customer-satisfied projects with smiles, happiness, joys, and charm. Click here and spread your joy with everyone!' />
+      </Head>
+      <PageBanner title='Projects' />
+      <ProjectList projects={projects} />
+      <LivelinkList links={links} />
+    </Layout>
+  )
 }
 
-export async function getServerSideProps() {
-	const res = await fetch(
-		'https://api.dribbble.com/v2/user/shots?access_token=1d41a60e6a59faef44cabd36abcf7003292ec9a0a49fc2db833e2c537bcb86b5'
-	)
+export async function getStaticProps() {
+  const res = await fetch('https://api.dribbble.com/v2/user/shots?access_token=1d41a60e6a59faef44cabd36abcf7003292ec9a0a49fc2db833e2c537bcb86b5')
 
-	const linkRes = await fetch(
-		'https://creativepeoplesdesign.com/admin/wp-json/wp/v2/link?per_page=100&&_embed'
-	)
+  const linkRes = await fetch('https://creativepeoplesdesign.com/admin/wp-json/wp/v2/link?per_page=100&&_embed')
 
-	const projects = await res.json()
-	const links = await linkRes.json()
+  const projects = await res.json()
+  const links = await linkRes.json()
 
-	return { props: { projects, links } }
+  return { props: { projects, links }, revalidate: 10 }
 }
 
 export default projects
